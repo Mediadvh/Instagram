d540c31ced76b93f82e739a0d82002a47ae7dd0c
Media Davarkhah <Media.dvh@gmail.com>
2021-09-10 10:37:25 +0430

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Media Davarkhah on 6/19/1400 AP.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
